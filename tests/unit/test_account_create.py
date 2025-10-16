a416c1b449d5e0aff20a3388d085e7ea57f9ec61
Mikołaj Modzelewski <m.modzelewski.968@studms.ug.edu.pl>
2025-10-16 13:58:39 +0200

    Feature1

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -6,3 +6,12 @@ class TestAccount:
         account = Account("John", "Doe")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.balance == 0
+        account = Account("Żenia", "Brzęczyszczykiewicz")
+        assert account.first_name == "Żenia"
+        assert account.last_name == "Brzęczyszczykiewicz"
+        assert account.balance == 0
+        account = Account("ᛗᚨᚱᛁᚨᚾ", "ᚲᛟᚹᚨᛚᛊᚲᛁ")
+        assert account.first_name == "ᛗᚨᚱᛁᚨᚾ"
+        assert account.last_name == "ᚲᛟᚹᚨᛚᛊᚲᛁ"
+        assert account.balance == 0
