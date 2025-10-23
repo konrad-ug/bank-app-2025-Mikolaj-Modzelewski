e728ffa287114009fcb08a5fd52c64738888e752
Mikołaj Modzelewski <m.modzelewski.968@studms.ug.edu.pl>
2025-10-23 11:53:23 +0200

    Feature 2&3

--- src/account.py
+++ src/account.py
@@ -1,5 +1,9 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel = "00000000000"):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = 0
+        if len(pesel) == 11:
+            self.pesel = pesel
+        else:
+            self.pesel = "invalid"
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -15,3 +15,13 @@ class TestAccount:
         assert account.first_name == "ᛗᚨᚱᛁᚨᚾ"
         assert account.last_name == "ᚲᛟᚹᚨᛚᛊᚲᛁ"
         assert account.balance == 0
+        account = Account("Marian", "Paździoch", "78030186911")
+        assert account.pesel == "78030186911"
+        account = Account("Ferdynand", "Kiepski", "77011142011")
+        assert account.pesel == "77011142011"
+    
+    def test_pesel_length(self):
+        account = Account("Lorem", "Ipsum", "dolor")
+        assert account.pesel == "invalid"
+        account = Account("Consectetur", "Latine", "12345678901")
+        assert account.pesel == "12345678901"
